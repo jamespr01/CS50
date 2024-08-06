7f9f2edc1e63cceaa2037d5ddfa1d14c936ae4eb
jamespr01 <j.rattanasatitkun@gmail.com>
2024-08-07 00:10:12 +0100

    uploaded mario2

--- Problem Set 1/mario2.c
+++ Problem Set 1/mario2.c
@@ -3,14 +3,14 @@
 
 void main(){
 
-    int i, j, k, h;
+    int i, j, k, h, l;
 
     // Asking for user to input height between 1 and 8
     do{
         h = get_int("Enter Height: ");
     } while (!(h > 0 && h < 9));
     
-    // Creating the stair
+    // Creating the pyramid
 
     // columns
     for(i = 0; i < h; i++){
@@ -20,6 +20,13 @@ void main(){
             printf(" ");
         }
         for(k = 0; j < i; j++){
+            printf("#");
+        }
+
+        printf(" ");
+
+        //left aligned #
+        for(l = 0; l < i+1; l++ ){
             printf("#");
         }
         printf("\n");        
