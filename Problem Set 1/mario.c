3514ac5fd7eac3daaedea6fb3e390936aa48079d
jamespr01 <j.rattanasatitkun@gmail.com>
2024-08-06 23:36:12 +0100

    Updated readme and uploading hello and mario assignment

---
+++ Problem Set 1/mario.c
@@ -0,0 +1,27 @@
+#include <cs50.c>
+#include <stdio.h>
+
+void main(){
+
+    int i, j, k, h;
+
+    // Asking for user to input height between 1 and 8
+    do{
+        h = get_int("Enter Height: ");
+    } while (!(h > 0 && h < 9));
+    
+    // Creating the pyramid
+
+    // columns
+    for(i = 0; i < h; i++){
+
+        //rows
+        for(j = h - i - 2; j > -1; j--){  
+            printf(" ");
+        }
+        for(k = 0; j < i; j++){
+            printf("#");
+        }
+        printf("\n");        
+    }
+}
